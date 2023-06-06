5c7585859977554cdf3a823c67d2fcd82561dd27
Frank Rocha <contato.frankrocha@gmail.com>
2023-06-06 12:51:07 -0300

    test: add create developer with valid data

---
+++ src/test/java/com/example/developerapi/domain/DeveloperServiceTest.java
@@ -0,0 +1,34 @@
+package com.example.developerapi.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static com.example.developerapi.constants.DeveloperConstants.DEVELOPER;
+import static com.example.developerapi.constants.DeveloperConstants.INVALID_DEVELOPER;;
+
+@ExtendWith(MockitoExtension.class)
+public class DeveloperServiceTest {
+
+	@InjectMocks
+	private DeveloperService developerService;
+
+	@Mock
+	private DeveloperRepository developerRepository;
+
+	@Test
+	public void createDeveloper_WithValidData_ReturnDeveloper() {
+		when(developerRepository.save(DEVELOPER)).thenReturn(DEVELOPER);
+
+		Developer sut = developerService.create(DEVELOPER);
+
+		assertThat(sut).isEqualTo(DEVELOPER);
+	}
+
+}
