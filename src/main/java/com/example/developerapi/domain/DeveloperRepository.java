66508c22236f25276a7275d80824bedeb9523e80
Frank Rocha <contato.frankrocha@gmail.com>
2023-06-06 12:47:12 -0300

    chore: add developer repository

---
+++ src/main/java/com/example/developerapi/domain/DeveloperRepository.java
@@ -0,0 +1,17 @@
+package com.example.developerapi.domain;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.QueryByExampleExecutor;
+
+import com.example.developerapi.domain.Developer;
+
+public interface DeveloperRepository extends CrudRepository<Developer, Long>, QueryByExampleExecutor<Developer> {
+	Optional<Developer> findByName(String name);
+
+	@Override
+	<S extends Developer> List<S> findAll(Example<S> example);
+}
